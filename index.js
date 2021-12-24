5b6d1f40e58b3bfafae46696d2c8b945260709cf
andres-condezo <ra.condezomonge@gmail.com>
2021-12-23 21:16:35 -0500

    Feat(index.js): [Create speakers function to show the speakers cards]

--- index.js
+++ index.js
@@ -41,3 +41,80 @@ function stickMenu() {
 
 window.onscroll = () => { stickMenu(); };
 
+// *****************
+// speakers-section
+// *****************
+
+const dataSpeakers = [
+  {
+    name: 'Miguel Ángel Durán',
+    twitter: '@midudev',
+    description: 'JavaScript programming developer, GitHub Star and Google Developer Expert',
+    image: './img/minudev.png',
+  },
+  {
+    name: 'Nuria Soriano',
+    twitter: '@nuria_codes',
+    description: 'Frontend category lead. I develop websites and sometimes video games.',
+    image: './img/nuria-soriano.png',
+  },
+  {
+    name: 'Maximilian Schwarzmüller',
+    twitter: '@maxedapps',
+    description: 'I teach & love coding: https://academind.com/courses',
+    image: './img/maximilian.png',
+  },
+  {
+    name: 'Fernando Herrera',
+    twitter: '@Fernando_Her85',
+    description: 'Full Stack developer and teacher.',
+    image: './img/fernando-herrera.png',
+  },
+  {
+    name: 'Oscar Barajas',
+    twitter: '@gndx',
+    description: 'Foundation Layer at @platzi. Microsoft MVP - Lead at Developer Circles from Facebook',
+    image: './img/oscar-barajas.png',
+  },
+  {
+    name: 'Jonathan Mircha',
+    twitter: '@jonmircha',
+    description: 'Professor. Web Programmer & Content Creator',
+    image: './img/jhon-mircha.png',
+  },
+];
+
+function createCard(el) {
+  return `
+  <figure class="img-container">
+    <img src="./img/img-bg-1.png" alt="" class="bg-img"/>
+    <img src="${el.image}" alt="${el.name} photo" class="speakers-img"/>
+  </figure>
+  <div class="speakers-text">
+    <div class="speakers-title">${el.name}</div>
+    <p class="speakers-subtitle">
+      ${el.twitter}
+    </p>
+    <p class="speakers-par">
+      ${el.description}
+    </p>
+  </div>
+`;
+}
+
+const speakers = dataSpeakers.map((el, index) => {
+  const article = document.createElement('article');
+  const maxCards = 2;
+  article.className = 'speakers-card';
+  if (index >= maxCards) {
+    article.classList.add('desktopCard');
+  }
+  article.innerHTML = createCard(el);
+  return article;
+});
+
+const speakersSection = document.getElementById('speakers');
+
+for (let i = 0; i < speakers.length; i += 1) {
+  speakersSection.appendChild(speakers[i]);
+}
